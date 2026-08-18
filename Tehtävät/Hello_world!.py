31a534b263e904d909c5463f682d6e07ad168f5e
kaap0p <kaap0p@outlook.com>
2026-08-18 10:46:59 +0300

    toimi pls

--- Tehtävät/Hello_world!.py
+++ Tehtävät/Hello_world!.py
@@ -1,1 +1,32 @@
-print("Hello World!")
+import time
+hello_ascii = """
+╔╗─╔╦═══╦╗──╔╗──╔═══╗╔╗
+║║─║║╔══╣║──║║──║╔═╗║║║
+║╚═╝║╚══╣║──║║──║║─║║║║
+║╔═╗║╔══╣║─╔╣║─╔╣║─║║╚╝
+║║─║║╚══╣╚═╝║╚═╝║╚═╝║╔╗
+╚╝─╚╩═══╩═══╩═══╩═══╝╚╝
+"""
+
+bye_ascii ="""
+★─▄█▀▀║░▄█▀▄║▄█▀▄║██▀▄║─★
+★─██║▀█║██║█║██║█║██║█║─★
+★─▀███▀║▀██▀║▀██▀║███▀║─★
+★───────────────────────★
+★───▐█▀▄─ ▀▄─▄▀ █▀▀──█───★
+★───▐█▀▀▄ ──█── █▀▀──▀───★
+★───▐█▄▄▀ ──▀── ▀▀▀──▄───★
+"""
+hello_or_bye = input("")
+if hello_or_bye.lower() in ["hello", "bye"]:
+    for i in range(1, 100):
+        print(f"{i}%")
+        time.sleep(0.1)
+    if hello_or_bye.lower() == "hello":
+        print(hello_ascii)
+    elif hello_or_bye.lower() == "bye":
+        print(bye_ascii)
+else:
+    print("Error bitch!")
+
+
---
+++ Tehtävät/Ympyrän pinta-ala.py
@@ -0,0 +1,5 @@
+import math
+pii = 3.141592653
+säde = int(input("anna ympyrän säde:"))
+
+print(pii * säde * säde)
